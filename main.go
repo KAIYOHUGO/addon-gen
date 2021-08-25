4c1f0e1da0b81e1b3022797c240fbfbf27101fae
KAIYOHUGO <ky.mc.minecraft@gmail.com>
2021-08-25 10:37:46 +0800

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/creasty/defaults"
+)
+
+func main() {
+	config := new(Config)
+	cf, err := os.Open("./manifest.json")
+	if err != nil {
+		panic(err)
+	}
+	json.NewDecoder().Decode(config)
+
+	defaults.MustSet(config)
+}
